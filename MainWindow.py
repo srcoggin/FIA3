c91ca0542bf1c0a009f24eac04ffe1a079c3cb00
Will Coggins <110357970+srcoggin@users.noreply.github.com>
2023-07-18 18:50:01 +1000

    Finished both Pages of Treatment, some more fix ups with Patients.
    Appointments is still left to code. Then some tweaks to the Wire frames need to be done. Then i need to format it in a document and write some documentations and annotations and I'm all done! I haven't even had the assesment for a week yet and the hardest part is almost over.

--- MainWindow.py
+++ MainWindow.py
@@ -4,6 +4,9 @@ from datastore import DataStore
 import datetime
 
 
+
+
+
 class MainWindow():
     def __init__(self):
         self.main_win = QMainWindow()
@@ -12,6 +15,7 @@ class MainWindow():
         self.ui.setupUi(self.main_win)
         self.currentTime = datetime.datetime.now()
         self.ui.stackedWidget.setCurrentWidget(self.ui.Home)
+      
 
         #Setting the "Good, (INSERT)" at the Home page
         self.EveningMorningButtons()
@@ -21,6 +25,7 @@ class MainWindow():
         self.ui.About_Banner_Button.clicked.connect(self.About)
         self.ui.Home_Banner_Button.clicked.connect(self.Home)
         self.ui.Patients_Banner_Button.clicked.connect(self.Patients)
+        self.ui.Treatments_Banner_Buttons.clicked.connect(self.Treatments)
 
 
         #Patient Page 1 Buttons
@@ -33,6 +38,17 @@ class MainWindow():
         self.ui.Previous_Page_Button_Patients2.clicked.connect(self.Patients)
         self.ui.Add_Patient_Button.clicked.connect(self.add_patient)
         self.ui.Update_Patient_Button.clicked.connect(self.Update_Patient)
+
+        #Treatment Page 1 Buttons
+        self.ui.Next_Page_Treatments.clicked.connect(self.NextPageTreatments)
+        self.ui.List_Of_Treatments_Button.clicked.connect(self.ShowListOfTreatments)
+        self.ui.Search_Treatments_Button.clicked.connect(self.SearchTreatments)
+        self.ui.Remove_Treatment_Button.clicked.connect(self.Remove_Treatment)
+
+        #Treatment Page 2 Buttons
+        self.ui.Previous_Page_Treatments.clicked.connect(self.Patients)
+        self.ui.Add_Treatment_Button.clicked.connect(self.add_treatment)
+        self.ui.Update_Treatment_Button.clicked.connect(self.Update_Treatment)
     
 
     #Opens the Window When Ran
@@ -51,6 +67,12 @@ class MainWindow():
     #Exits the program
     def Exit(self):
         exit()
+
+    def Treatments(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.Treatments)       
+
+    def NextPageTreatments(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.Treatments_Page_2) 
 
     def About(self):
         msg = QMessageBox()
@@ -72,6 +94,55 @@ class MainWindow():
         msg.exec()
 
 
+    def SearchTreatments(self):
+        Treatment_id = self.ui.Treatment_id_Search.text()
+        Treatments = self.ds.Matching_Treatment_id()
+        if Treatment_id not in Treatments:
+            self.error()
+        else:
+            Treatment_info = self.ds.select_matching_treatment(self.ui.Treatment_id_Search.text())
+            info = ""
+            for i in Treatment_info:
+                info += """
+Treatment Id: {}
+Treatment Name: {}
+Treatment Desc: {}
+Treatment Cost: {}
+                """.format(i[0], i[3], i[2], i[1])
+            self.ui.Search_Treatment_Label.setText(f"""
+Treatment Info:
+{info}       
+""")
+
+    def ShowListOfTreatments(self):
+        List = self.ds.ListOfTreatments()
+        name = ""
+        for i in List:
+            name += (f"{i[0]}, Id: {i[1]} \n")
+        self.ui.List_Of_Treatments_Label.setText(f"""
+List Of Treatments:                   
+{name}       
+""")
+        
+    def Remove_Treatment(self):
+        Treatment_id = self.ui.Remove_Treatment_Id.text()
+        Treatments = self.ds.Matching_Treatment_id()
+        msg = QMessageBox()
+        msg.setText("Are you sure you want to do this?")
+        msg.setWindowTitle("Are you sure!")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        button_clicked = msg.exec()
+        if button_clicked == QMessageBox.Yes:
+            if Treatment_id not in Treatments:
+                self.error()
+            else:
+                self.ds.Delete_Treatment(id = self.ui.Remove_Treatment_Id.text())
+                self.succesful_popup()
+        elif button_clicked == QMessageBox.No:
+            QMessageBox.close
+        else:
+            QMessageBox.close
+
     def NextPagePatients(self):
         self.ui.stackedWidget.setCurrentWidget(self.ui.Patients2)
 
@@ -88,20 +159,19 @@ class MainWindow():
             name += (f"{i[0]}, Id: {i[1]} \n")
         self.ui.List_Of_Patients_Label.setText(f"""
 List Of Patients:
-
 {name}                      
 """)
 
     def error(self):
         msg = QMessageBox()
-        msg.setWindowTitle("Opersation Failed")
-        msg.setText("Id Does Not Exsist!")
+        msg.setWindowTitle("Operation Failed")
+        msg.setText("    That Id Does Not Exsist!        ")
         msg.setStandardButtons(QMessageBox.Close)
         msg.exec()
 
     def SearchPatients(self):
         patient_id = self.ui.Patient_Id_Search_Patient.text()
-        patients = self.ds.Matching_id()
+        patients = self.ds.Matching_Patient_id()
         if patient_id not in patients:
             self.error()
         else:
@@ -129,9 +199,31 @@ Patient Info:
         msg.setStandardButtons(QMessageBox.Close)
         msg.exec()
 
+    def add_treatment(self):
+        self.ds.Add_Treatment(
+            id = self.ui.Add_Treatment_Id.text(),
+            Cost = self.ui.Add_Treatment_Cost.text(),
+            Description = self.ui.Add_Treatment_Desc.text(),
+            TreatmentName = self.ui.Add_Treatment_Name.text()
+        )
+        self.ui.Add_Treatment_Id.clear()
+        self.ui.Add_Treatment_Cost.clear()
+        self.ui.Add_Treatment_Desc.clear()
+        self.ui.Add_Treatment_Name.clear()
+        self.succesful_popup()
+
 
     def add_patient(self):
-        self.ds.Add_Patient(id = self.ui.Add_Patient_Id.text(), FirstName = self.ui.Add_Patient__FirstName.text(), LastName = self.ui.Add_Patient_LastName.text(), Height = self.ui.Add_Patient_Height.text(), Weight = self.ui.Add_Patient_Weight.text(), Address = self.ui.Add_Patient_Address.text(), Amountoftreatmentstaken = self.ui.Add_Num_Of_treat.text())
+        self.ds.Add_Patient(
+
+            id = self.ui.Add_Patient_Id.text(), 
+            FirstName = self.ui.Add_Patient__FirstName.text(), 
+            LastName = self.ui.Add_Patient_LastName.text(), 
+            Height = self.ui.Add_Patient_Height.text(), 
+            Weight = self.ui.Add_Patient_Weight.text(), 
+            Address = self.ui.Add_Patient_Address.text(), 
+            Amountoftreatmentstaken = self.ui.Add_Num_Of_treat.text()
+        )
         self.ui.Add_Patient_Id.clear()
         self.ui.Add_Patient__FirstName.clear()
         self.ui.Add_Patient_LastName.clear()
@@ -143,71 +235,134 @@ Patient Info:
 
     def delete_patient(self):
         patient_id = self.ui.Patient_Id_Remove_Patient.text()
-        patients = self.ds.Matching_id()
-        if patient_id not in patients:
-            self.error()
+        patients = self.ds.Matching_Patient_id()
+        msg = QMessageBox()
+        msg.setText("Are you sure you want to do this?")
+        msg.setWindowTitle("Are you sure!")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        button_clicked = msg.exec()
+        if button_clicked == QMessageBox.Yes:
+            if patient_id not in patients:
+                self.error()
+            else:
+                self.ds.Delete_Patient(id = self.ui.Patient_Id_Remove_Patient.text())
+                self.succesful_popup()
+        elif button_clicked == QMessageBox.No:
+            QMessageBox.close
         else:
-            self.ds.Delete_Patient(id = self.ui.Patient_Id_Remove_Patient.text())
-            self.succesful_popup()
+            QMessageBox.close
+
+
+
+    def Update_Treatment(self):
+        Treatment_id = self.ui.Update_Patient_Id.text()
+        Treatments = self.ds.Matching_Treatment_id()
+        msg = QMessageBox()
+        msg.setText("Are you sure you want to do this?")
+        msg.setWindowTitle("Are you sure!")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        button_clicked = msg.exec()
+        if button_clicked == QMessageBox.Yes:
+            if Treatment_id not in Treatments:
+                self.error()
+            else:
+                if self.ui.Update_Treatment_Cost_Radio.isChecked():
+                    id = self.ui.Update_Treatment_Id.text()
+                    UpdatedValue = self.ui.Update_Treatment_Cost.text()
+                    UpdatedSelection = 1
+                    self.ds.update_treatment(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Treatment_Cost.clear()
+                    self.succesful_popup()
+
+                elif self.ui.Update_Treatment_Desc_Radio.isChecked():
+                    id = self.ui.Update_Treatment_Id.text()
+                    UpdatedValue = self.ui.Update_Treatment_Desc.text()
+                    UpdatedSelection = 2
+                    self.ds.update_treatment(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Treatment_Desc.clear()
+                    self.succesful_popup()
+
+                elif self.ui.Update_Treatment_Name_Radio.isChecked():
+                    id = self.ui.Update_Treatment_Id.text()
+                    UpdatedValue = self.ui.Update_Treatment_Name.text()
+                    UpdatedSelection = 3
+                    self.ds.update_treatment(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Treatment_Name.clear()
+                    self.succesful_popup()
+
+        elif button_clicked == QMessageBox.No:
+            QMessageBox.close
+        else:
+            QMessageBox.close
 
 
     def Update_Patient(self):
         patient_id = self.ui.Update_Patient_Id.text()
         patients = self.ds.Matching_id()
-        if patient_id not in patients:
-            self.error()
+        msg = QMessageBox()
+        msg.setText("Are you sure you want to do this?")
+        msg.setWindowTitle("Are you sure!")
+        msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        button_clicked = msg.exec()
+        if button_clicked == QMessageBox.Yes:
+            if patient_id not in patients:
+                self.error()
+            else:
+                if self.ui.Patient_Update_Name_Radio.isChecked():
+                    id = self.ui.Update_Patient_Id.text()
+                    UpdatedValue = self.ui.Update_Patient_FirstName.text()
+                    UpdatedSelection = 1
+                    self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Patient_FirstName.clear()
+                    self.succesful_popup()
+
+
+                elif self.ui.Patient_Update_LastName_Radio.isChecked():
+                    id = self.ui.Update_Patient_Id.text()
+                    UpdatedValue = self.ui.Update_Patient_LastName.text()
+                    UpdatedSelection = 2
+                    self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Patient_LastName.clear()
+                    self.succesful_popup()
+
+                elif self.ui.Patient_Update_Address_Radio.isChecked():
+                    id = self.ui.Update_Patient_Id.text()
+                    UpdatedValue = self.ui.Update_Patient_Address.text()
+                    UpdatedSelection = 3
+                    self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Patient_Address.clear()
+                    self.succesful_popup()
+
+
+                elif self.ui.Patient_Update_Height_Radio.isChecked():
+                    id = self.ui.Update_Patient_Id.text()
+                    UpdatedValue = self.ui.Update_Patient_Height.text()
+                    UpdatedSelection = 4
+                    self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Patient_Height.clear()
+                    self.succesful_popup()
+
+
+                elif self.ui.Patient_Update_Weight_Radio.isChecked():
+                    id = self.ui.Update_Patient_Id.text()
+                    UpdatedValue = self.ui.Update_Patient_Weight.text()
+                    UpdatedSelection = 5
+                    self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Patient_Weight.clear()
+                    self.succesful_popup()
+
+
+                elif self.ui.Patient_Update_Treatments_Radio.isChecked():
+                    id = self.ui.Update_Patient_Id.text()
+                    UpdatedValue = self.ui.Update_Num_Of_Treatments.text()
+                    UpdatedSelection = 6
+                    self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
+                    self.ui.Update_Num_Of_Treatments.clear()
+                    self.succesful_popup()
+        elif button_clicked == QMessageBox.No:
+            QMessageBox.close
         else:
-            if self.ui.Patient_Update_Name_Radio.isChecked():
-                id = self.ui.Update_Patient_Id.text()
-                UpdatedValue = self.ui.Update_Patient_FirstName.text()
-                UpdatedSelection = 1
-                self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
-                self.ui.Update_Patient_FirstName.clear()
-                self.succesful_popup()
-
-
-            elif self.ui.Patient_Update_LastName_Radio.isChecked():
-                id = self.ui.Update_Patient_Id.text()
-                UpdatedValue = self.ui.Update_Patient_LastName.text()
-                UpdatedSelection = 2
-                self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
-                self.ui.Update_Patient_LastName.clear()
-                self.succesful_popup()
-
-            elif self.ui.Patient_Update_Address_Radio.isChecked():
-                id = self.ui.Update_Patient_Id.text()
-                UpdatedValue = self.ui.Update_Patient_Address.text()
-                UpdatedSelection = 3
-                self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
-                self.ui.Update_Patient_Address.clear()
-                self.succesful_popup()
-
-
-            elif self.ui.Patient_Update_Height_Radio.isChecked():
-                id = self.ui.Update_Patient_Id.text()
-                UpdatedValue = self.ui.Update_Patient_Height.text()
-                UpdatedSelection = 4
-                self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
-                self.ui.Update_Patient_Height.clear()
-                self.succesful_popup()
-
-
-            elif self.ui.Patient_Update_Weight_Radio.isChecked():
-                id = self.ui.Update_Patient_Id.text()
-                UpdatedValue = self.ui.Update_Patient_Weight.text()
-                UpdatedSelection = 5
-                self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
-                self.ui.Update_Patient_Weight.clear()
-                self.succesful_popup()
-
-
-            elif self.ui.Patient_Update_Treatments_Radio.isChecked():
-                id = self.ui.Update_Patient_Id.text()
-                UpdatedValue = self.ui.Update_Num_Of_Treatments.text()
-                UpdatedSelection = 6
-                self.ds.update_patient(id, UpdatedSelection, UpdatedValue)
-                self.ui.Update_Num_Of_Treatments.clear()
-                self.succesful_popup()
+            QMessageBox.close
 
 
 
--- datastore.py
+++ datastore.py
@@ -5,13 +5,35 @@ import sqlite3
 class DataStore:
     def __init__(self):
         self.db_file = os.path.join(os.getcwd(), "FIA3.db")
-        self.db = sqlite3.connect(self.db_file)
+        self.db = sqlite3.connect(r"C:\Users\Will\Documents\GitHub\FIA3\FIA3.db")
         self.cursor = self.db.cursor()
 
     def __del__(self):
         self.db.close()
 
-    def Matching_id(self):
+
+    def ListOfTreatments(self):
+        self.cursor.execute(
+            """
+                SELECT TreatmentName, Id FROM Treatment
+            """
+        )
+        QList = self.cursor.fetchall()
+        return QList
+
+    def Matching_Treatment_id(self):
+        self.cursor.execute(
+            """
+                SELECT id FROM Treatment
+            """
+        )
+        QList = self.cursor.fetchall()
+        id = ""
+        for i in QList:
+            id +="{}".format(i)
+        return id
+
+    def Matching_Patient_id(self):
         self.cursor.execute(
             """
                 SELECT id FROM Patients
@@ -23,8 +45,16 @@ class DataStore:
             id +="{}".format(i)
         return id
 
-
-
+    def select_matching_treatment(self, treatmentid):
+        self.cursor.execute(
+            """
+                SELECT * FROM Treatment
+                WHERE Id LIKE :treatmentid
+            """,
+            {"treatmentid": treatmentid}
+        )
+        QList = self.cursor.fetchall()
+        return QList
 
     def select_matching_user(self, patientid):
         self.cursor.execute(
@@ -37,6 +67,18 @@ class DataStore:
         )
         QList = self.cursor.fetchall()
         return QList
+    
+
+    def Add_Treatment(self, id, TreatmentName, Description, Cost):
+        self.cursor.execute(
+            """
+                INSERT INTO Treatment (id, Cost, Description, TreatmentName)
+                VALUES (:id, :Cost, :Description, :TreatmentName)
+            """,
+            {"id": id, "Cost": Cost, "Description": Description, "TreatmentName": TreatmentName}
+        )
+        self.db.commit()
+
 
     def Add_Patient(self, id, FirstName, LastName, Height, Weight, Address, Amountoftreatmentstaken):
         self.cursor.execute(
@@ -66,6 +108,51 @@ class DataStore:
         {"id": id}
         )
         self.db.commit()
+
+
+    def Delete_Treatment(self, id):
+        self.cursor.execute(
+            """
+                DELETE FROM Treatment
+                WHERE (:id) = id
+            """,
+            {"id": id}
+        )
+        self.db.commit()
+
+
+
+    def update_treatment(self, id, UpdatetdSelection, UpdatedValue):
+        if UpdatetdSelection == 1:
+            self.cursor.execute(
+                """
+                    UPDATE Treatment SET Cost = (:UpdatedValue)
+                    WHERE (:id) = id
+                """,
+                {"UpdatedValue": UpdatedValue, "id": id}
+            )
+            self.db.commit()
+
+        elif UpdatetdSelection == 2:
+            self.cursor.execute(
+                """
+                    UPDATE Treatment SET Description = (:UpdatedValue)
+                    WHERE (:id) = id
+                """,
+                {"UpdatedValue": UpdatedValue, "id": id}
+            )
+            self.db.commit()
+
+        elif UpdatetdSelection == 3:
+            self.cursor.execute(
+                """
+                    UPDATE Treatment SET TreatmentName = (:UpdatedValue)
+                    WHERE (:id) = id
+                """,
+                {"UpdatedValue": UpdatedValue, "id": id}
+            )
+            self.db.commit()
+
 
     def update_patient(self, id, UpdatedSelection, UpdatedValue):
         print(UpdatedSelection)
